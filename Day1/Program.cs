6dbe743d7e5364bd70ef10bd722970c8dd7b18f9
CodesLikeChicken <fakeAddres@fakeDomain.com>
2022-12-01 08:51:35 -0800

    Implement day 1 part 2

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -28,3 +28,42 @@ if (currentTotalCals > maxTotalCals)
 }
 
 Console.WriteLine($"Max total cals = {maxTotalCals}");
+
+// Part 2, get top three total cals.
+
+var allCalTotals = new List<int>();
+
+// Reset data
+currentTotalCals = 0;
+
+foreach (var input in InputParser.ParseLines("./Inputs/Input.txt"))
+{
+    if (string.IsNullOrEmpty(input))
+    {
+        allCalTotals.Add(currentTotalCals);
+
+        currentTotalCals = 0;
+        continue;
+    }
+
+    currentTotalCals += Int32.Parse(input);
+}
+
+allCalTotals.Add(currentTotalCals);
+
+// Sort decending
+allCalTotals.Sort((x, y) => y.CompareTo(x));
+
+var topThreeTotal = 0;
+for(int i = 0; i < 3; i++)
+{
+    var totalCal = allCalTotals[i];
+
+    Console.WriteLine($"Max total cals {i + 1} = {totalCal}");
+
+    topThreeTotal += totalCal;
+}
+
+Console.WriteLine($"Top three total = {topThreeTotal}");
+
+Console.WriteLine("All Done");
