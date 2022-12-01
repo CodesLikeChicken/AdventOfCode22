61e4b04331fd0bc3688a18ad92ec1c32415cf473
CodesLikeChicken <fakeAddres@fakeDomain.com>
2022-12-01 08:33:14 -0800

    Implement day 1 part 1.

---
+++ Day1/Program.cs
@@ -0,0 +1,30 @@
+﻿using AdventOfCode;
+
+Console.WriteLine("Hello, Day 1!");
+
+var maxTotalCals= 0;
+var currentTotalCals = 0;
+
+foreach(var input in InputParser.ParseLines("./Inputs/Input.txt"))
+{
+    if(string.IsNullOrEmpty(input))
+    {
+        if(currentTotalCals > maxTotalCals)
+        {
+            maxTotalCals = currentTotalCals;
+        }
+
+        currentTotalCals = 0;
+        continue;
+    }
+
+    currentTotalCals += Int32.Parse(input);
+}
+
+// Check for any left over after last line is parsed.
+if (currentTotalCals > maxTotalCals)
+{
+    maxTotalCals = currentTotalCals;
+}
+
+Console.WriteLine($"Max total cals = {maxTotalCals}");
