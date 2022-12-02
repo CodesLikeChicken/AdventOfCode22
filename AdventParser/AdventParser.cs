aac8ff5b82ac61ee5597551f3854dd99d7cda8df
CodesLikeChicken <fakeAddres@fakeDomain.com>
2022-12-01 23:53:59 -0800

    Parser code clean up.

--- AdventParser/AdventParser.cs
+++ AdventParser/AdventParser.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Text;
+﻿using System.Text;
 
 namespace AdventOfCode
 {
@@ -27,26 +26,6 @@ namespace AdventOfCode
             return tokenizedString.ToString().Split(finalToken).Where(value => !string.IsNullOrEmpty(value)).ToList();
         }
 
-        public static void Parse(string inputFile, List<string> inputTokens)
-        {
-            var inputStream = new StreamReader(inputFile);
-
-            if (!string.IsNullOrEmpty(inputFile))
-            {
-                var inputLines = new List<string>();
-                var inputLinesByParameter = new List<List<string>>();
-
-                string inputLine;
-                while ((inputLine = inputStream.ReadLine()) != null)
-                {
-                    inputLines.Add(inputLine);
-                    inputLinesByParameter.Add(SplitInput(inputLine, inputTokens));
-                }
-
-                inputStream.Close();
-            }
-        }
-
         public static IEnumerable<string> ParseLines(string inputFile)
         {
             var inputStream = new StreamReader(inputFile);
@@ -62,19 +41,5 @@ namespace AdventOfCode
                 inputStream.Close();
             }
         }
-
-        //protected void PrintEstimatedCompletion(long total, long numberComplete, TimeSpan elapsedTime)
-        //{
-        //    var percentDone = (double)numberComplete / total;
-
-        //    var completionTime = new TimeSpan();
-        //    for (double timeSpanIndex = 0; timeSpanIndex < 1 / percentDone; ++timeSpanIndex)
-        //    {
-        //        completionTime += elapsedTime;
-        //    }
-
-        //    Console.WriteLine($"{numberComplete} ({percentDone * 100}%) finished in {elapsedTime}");
-        //    Console.WriteLine($"  {completionTime} projected total run time.\n");
-        //}
     }
 }
