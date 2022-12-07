6769aa2cbe694afbed4f055685c2e442f6052154
CodesLikeChicken <fakeAddres@fakeDomain.com>
2022-12-06 20:55:12 -0800

    Add starting point for day 7.

---
+++ Day7/Program.cs
@@ -0,0 +1,22 @@
+﻿using AdventOfCode;
+
+var day = -1;
+int answer = 1337;
+
+Console.WriteLine($"Hello, Day {day}");
+
+// Do part 1 here;
+foreach(var input in InputParser.ParseLines("./Inputs/Input.txt"))
+{
+    
+}
+
+Console.WriteLine($"Part 1 answer = {answer}");
+
+// Do part 2 here:
+foreach (var input in InputParser.ParseLines("./Inputs/Input.txt"))
+{
+
+}
+
+Console.WriteLine($"Part 2 answer = {answer}");
